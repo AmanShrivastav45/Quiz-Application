2e11ea6deeb2c793e25e0c12a2ccc50e5dbcb1ed
Aman-Shrivastav <amanshrivastav4501@gmail.com>
2024-02-03 19:14:51 +0530

    minor functionalities added

--- frontend/src/pages/Quiz.tsx
+++ frontend/src/pages/Quiz.tsx
@@ -2,51 +2,75 @@ import React, { useState, useEffect } from "react";
 import "../../src/index.css";
 import "../fonts/stylesheet.css";
 
-interface Option {
-  label: string;
-  text: string;
-}
+export default function Quiz() {
+  interface Option {
+    label: string;
+    text: string;
+  }
 
-const QuestionGrid: React.FC<{ totalQuestions: number }> = ({
-  totalQuestions,
-}) => {
-  return (
-    <div className="grid grid-cols-5 gap-6 ">
-      {Array.from({ length: totalQuestions }, (_, index) => (
-        <button
-          key={index + 1}
-          className="m-1  border-solid border border-black text-[#2a2a2a] h-[50px] w-[50px] grid place-items-center  rounded  focus:ring-2 ring-blue-500 focus:bg-sky-100  Apercu-Medium text-base "
-        >
-          {index + 1}
-        </button>
-      ))}
-    </div>
+  const total: number = 5;
+
+  const QuestionGrid: React.FC<{ totalQuestions: number }> = ({
+    totalQuestions,
+  }) => {
+    return (
+      <div className="grid grid-cols-5 gap-6 ">
+        {Array.from({ length: totalQuestions }, (_, index) => (
+          <button
+            style={{
+              backgroundColor: visited[index] ? "#d1d5db" : "",
+              fontWeight: visited[index] ? "bold": "medium",
+            }}
+            onClick={() => handleQuestionNumber(index)}
+            key={`question-${index + 1}`}
+            className="m-1 border-solid border border-black text-[#2a2a2a] h-[50px] w-[50px] grid place-items-center rounded focus:ring-2 ring-blue-500 focus:bg-sky-100 Apercu-Medium text-base"
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    );
+  };
+
+  const [QuestionNumber, setQuestionNumber] = useState(1);
+  const [visited, setVisited] = useState(Array(total).fill(false));
+  visited[0] = true;
+  const [NotVisited, setNotVisited] = useState(total-1);
+
+  const handleQuestionNumber = (index: number) => {
+    setQuestionNumber(index + 1);
+    if (!visited[index]) {
+      setVisited((prevVisited) => {
+        const newVisited = [...prevVisited];
+        newVisited[index] = true;
+        setNotVisited(NotVisited-1);
+        return newVisited;
+      });    
+    }
+  };
+
+  
+  const QuizOption: React.FC<{ option: Option }> = ({ option }) => (
+    <button  className="w-full min-h-[70px] mb-4 rounded border border-black text-left pl-4 Apercu-Regular text-xl focus:ring-2 ring-blue-500 focus:bg-sky-100 py-2">
+      <span className="mr-2">{option.label}:</span>
+      {option.text}
+    </button>
   );
-};
+  
 
-const QuizOption: React.FC<{ option: Option }> = ({ option }) => (
-  <button className="w-full min-h-[70px] mb-4 rounded border border-black text-left pl-4 Apercu-Regular text-xlfocus:ring-2 ring-blue-500 focus:bg-sky-100  py-2">
-    <span className="mr-2">{option.label}:</span>
-    {option.text}
-  </button>
-);
-
-const Quiz: React.FC = () => {
+  // Timer
   const [currentTime, setCurrentTime] = useState(new Date());
-
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
-
-    // Clear interval on component unmount
     return () => clearInterval(intervalId);
   }, []);
 
   let Question =
-    "A train can travel 50% faster than a car. Both start from point A at the same time and reach point B 75 kms away from A at the same time. On the way, however, the train lost about 12.5 minutes while stopping at the stations. The speed of the car is:";
-  let QuestionNo: number = 56;
-  let option1: string = "100kmph"
+    "A train can travel 50% faster than a car. The speed of the car is:";
+
+  let option1: string = "100kmph";
   let option2: string = "110kmph";
   let option3: string = "120kmph";
   let option4: string = "130kmph";
@@ -57,12 +81,12 @@ const Quiz: React.FC = () => {
     { label: "C", text: option3 },
     { label: "D", text: option4 },
   ];
+
   const unattempted: number = 48;
   const attempted: number = 52;
   const review: number = 7;
-  const notVisited: number = 10;
-  const total: number = 100;
   const username: string = "Aman Shrivastav";
+
   return (
     <div className="w-full h-screen flex items-center justify-center px-16 py-4">
       <div className="h-full w-[75%] overflow-hidden">
@@ -74,7 +98,7 @@ const Quiz: React.FC = () => {
         </div>
         <div className="h-[28%] mt-4 border-black border w-full mb-4 pb-2 px-4 rounded Apercu-Regular text-xl">
           <div className="sticky top-0 h-12 w-full bg-[#fff0e5] pt-3 pb-2 Apercu-Bold">
-            Question No: {QuestionNo}
+            Question No: {QuestionNumber}
           </div>
           <div>{Question}</div>
         </div>
@@ -83,12 +107,11 @@ const Quiz: React.FC = () => {
           <div className="sticky top-0 h-12 Apercu-Bold text-xl w-full bg-[#fff0e5] mb-2 pt-3 pb-2 ">
             Select any one option below:
           </div>
-          <div className="Apercu-Medium text-xl">
-          {options.map((option, index) => (
-            <QuizOption key={index} option={option}/>
-          ))}
+          <div className="Apercu-Medium text-xl p-1">
+            {options.map((option, index) => (
+              <QuizOption key={index} option={option}  />
+            ))}
           </div>
-
         </div>
         <div className="flex justify-between align-center ">
           <button className="h-12 Apercu-Medium overflow-hidden border-solid border transition duration-200 ease-in-out border-[#1e1b4b] text-white bg-[#1e1b4b] hover:bg-[#172554] focus:bg-[#172554] rounded text-base px-4 lg:px-5 py-2 lg:py-2.5  focus:outline-none">
@@ -101,9 +124,11 @@ const Quiz: React.FC = () => {
       </div>
       <div className="h-full w-[25%] ml-4 overflow-hidden">
         <div className="h-12 display-time Apercu-Bold text-xl text-right flex justify-end items-center">
-          Time Left: <span className="w-[100px] h-full text-center py-2.5">{" "}
-          {currentTime.toLocaleTimeString("en-US", { hour12: false })}</span>
-          
+          Time Left:{" "}
+          <span className="w-[100px] h-full text-center py-2.5">
+            {" "}
+            {currentTime.toLocaleTimeString("en-US", { hour12: false })}
+          </span>
         </div>
         <div className="border border-black h-[28%] rounded mt-4 mb-4">
           <div className="sticky top-0 h-12 w-full bg-[#fff0e5] pt-2 pb-2 text-center border-b border-black Apercu-Bold mb-4 text-xl px-4">
@@ -129,7 +154,7 @@ const Quiz: React.FC = () => {
               </div>
               <div className="flex justify-start align-center mb-4">
                 <div className="border-solid border border-black text-[#2a2a2a] h-[40px]  w-[40px] grid place-items-center  rounded  Apercu-Bold text-base ">
-                  {notVisited}
+                  {NotVisited}
                 </div>
                 <span className="Apercu-Regular text-base py-2 ml-2">
                   Not visited
@@ -157,7 +182,6 @@ const Quiz: React.FC = () => {
               <div className="flex justify-start align-center mb-4">
                 <div className="border-solid border  border-black text-[#16a34a] h-[40px]  w-[40px] grid place-items-center  rounded  Apercu-Bold text-base ">
                   {unattempted}
-                  
                 </div>
                 <span className="Apercu-Regular text-base py-2 ml-2">
                   Marked-Attempted
@@ -167,7 +191,7 @@ const Quiz: React.FC = () => {
           </div>
         </div>
         <div className="h-[57%] overflow-auto rounded p-6 mb-4 border-solid border border-black ">
-          <QuestionGrid totalQuestions={total} />
+        <QuestionGrid totalQuestions={total}/>
         </div>
         <button className="h-12 w-full Apercu-Medium overflow-hidden border-solid border transition duration-200 ease-in-out border-[#1e1b4b] text-white bg-[#1e1b4b] hover:bg-[#172554] focus:bg-[#172554] rounded text-base px-4 lg:px-5 py-2 lg:py-2.5 focus:outline-none">
           Submit Test
@@ -175,6 +199,4 @@ const Quiz: React.FC = () => {
       </div>
     </div>
   );
-};
-
-export default Quiz;
+}
